6e2fb9ec2e37167bd873b96a39958bf03ee1ac0e
Nidhi Priyadarshi <nidhip2@birlasoft.com>
2021-06-15 13:39:28 +0530

    pincode 30% done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PincodeComponent } from './pincode/pincode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PincodeComponent
   ],
   imports: [
     BrowserModule,
