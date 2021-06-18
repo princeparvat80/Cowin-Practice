24625755d32d533657bd6b270604249d9bee4490
Deepali Sehgal <deepalis1@birlasoft.com>
2021-06-18 08:07:52 +0530

    working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectByDistrictComponent } from './select-by-district/select-by-district.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'district',
+    component: SelectByDistrictComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectByDistrictComponent } from './select-by-district/select-by-district.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectByDistrictComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
